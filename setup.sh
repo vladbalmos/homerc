76e62af3e45c15a9855a5c9573a9bb37daf8fc84
Vlad Balmos <vlad.balmos@gmail.com>
2016-03-16 20:28:46 +0200

    Add openbox configuration && update setup script

--- setup.sh
+++ setup.sh
@@ -3,15 +3,18 @@
 CURRENT_PATH=$(pwd)
 
 # tmux
-ln -s $CURRENT_PATH/tmux.conf $HOME/.tmux.conf
-echo -e alias tmux=\'tmux -2\' >> ~/.bash_aliases
+[ -f ~/.tmux.conf ] || [ -h ~/.tmux.conf ] || ln -s $CURRENT_PATH/tmux.conf ~/.tmux.conf
+grep tmux ~/.bash_aliases > /dev/null 2>&1 || echo -e alias tmux=\'tmux -2\' >> ~/.bash_aliases
 
 # bashrc
-ln -s -f $CURRENT_PATH/.bashrc $HOME/.bashrc
+[ -f ~/.bashrc ] || [ -h ~/.bashrc ] || ln -s $CURRENT_PATH/.bashrc ~/.bashrc
 
 # zsh
-ln -s -f $CURRENT_PATH/.zshrc $HOME/.zshrc
+[ -f ~/.zshrc ] || [ -h ~/.zshrc ] || ln -s $CURRENT_PATH/.zshrc ~/.zshrc
 
 # tmux plugin manager
 [ -d ~/.tmux/plugins/tpm ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# openbox configuration
+[ -d ~/.config/openbox ] || [ -h ~/.config/openbox ] || (cd ~/.config && ln -s $CURRENT_PATH/.config/openbox)
 
